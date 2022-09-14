1fd63223ecb161a0373aaa68d4aa45299ec28f63
Sarah Rajab <sarah.rajab94@gmail.com>
2022-09-14 14:29:35 +0300

    fix db connection

--- app/config.py
+++ app/config.py
@@ -24,7 +24,7 @@ if ENV == 'prod':
     PG_USER = 'hikma_prod'
     PG_PASSWORD = os.environ['DB_PASSWORD']
     PG_HOST = '10.114.32.2'
-    PG_DB = 'hikma-prod'
+    PG_DB = 'hikma_prod'
     PHOTOS_STORAGE_BUCKET = os.environ['PHOTOS_STORAGE_BUCKET']
     EXPORTS_STORAGE_BUCKET = os.environ['EXPORTS_STORAGE_BUCKET']
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
